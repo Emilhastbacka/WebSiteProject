8902bb3d1672c4d90d0c20c247c66b111bad0606
Emilhastbacka <emil.hastbacka1013@gmail.com>
2022-03-16 13:39:13 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+function scrollToHome() {
+    //window.scrollTo(0, document.body.scrollHeight)
+    window.scroll({
+        top: 0 * window.innerHeight / 100,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToAbout() {
+    //window.scrollTo(0, document.body.scrollHeight)
+    window.scroll({
+        top: 100 * window.innerHeight / 100,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToPortfolio() {
+    //window.scrollTo(0, document.body.scrollHeight)
+    window.scroll({
+        top: 200 * window.innerHeight / 100,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+function scrollToContact() {
+    //window.scrollTo(0, document.body.scrollHeight)
+    window.scroll({
+        top: 300 * window.innerHeight / 100,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+
